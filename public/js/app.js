696a4e927da0d349ca198c16f7cae73abaa08a01
Abhijit Malvadkar <abhijit@sorigin.com>
2023-02-21 16:41:07 +0530

    Deleted Console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client Side JavaScript File Is Loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
